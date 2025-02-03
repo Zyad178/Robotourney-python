aebd9d837a4e33a9171e37c413d63125609c267a
78-bit <abdallag1772001@gmail.com>
2025-02-03 17:19:06 +0200

    Delete Abdalla/input.py

--- Abdalla/input.py
+++
@@ -1,6 +0,0 @@
-name = input("name:  ")
-age = int(input("your age:  "))
-hobby = input("hobby:  ")
-classe = input("class")  # e added to class to not confuse with the class function
-school = input("school")
-print(f"my name is {name}, i am  {age} years old, my hobby is {hobby},my school is {school} my class is {classe}")
